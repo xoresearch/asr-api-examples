2a1fa8cb26c0d4e32b9388da8e2ddb35765e2e74
danek <danek.kulik@icloud.com>
2018-06-22 00:54:34 +0300

    add example for 'recognize' method

---
+++ recognize/recognize.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#-------------------------------------------------------------------------------------------------
+# Load voice data as a base64 encoded string
+#-------------------------------------------------------------------------------------------------
+voice_data=$(base64 --wrap=0 "${PWD}/../voices/short_voice.flac")
+
+#-------------------------------------------------------------------------------------------------
+# Compose request body
+#-------------------------------------------------------------------------------------------------
+request_body="{ \"signal\": \"${voice_data}\", \"language_code\": \"en-US\", \"execute_beam_search\": false }"
+
+#-------------------------------------------------------------------------------------------------
+# Execute remote call
+#-------------------------------------------------------------------------------------------------
+echo -En "${request_body}" | curl \
+	--verbose \
+	--max-time 60 \
+	--request POST \
+	--header "Content-Type: application/json; charset=utf-8" \
+	--data-binary @- \
+	"https://asr.sapiensapi.com/v1/speech:recognize"
